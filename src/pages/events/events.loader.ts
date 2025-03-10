cb306147f7b8177e7ce9ac35cb70ae63a1b1b5ba
Kún Ákos <kunaakos@gmail.com>
2025-03-10 12:54:15 +0100

    add crucial comment on loader deps

--- src/pages/events/events.loader.ts
+++ src/pages/events/events.loader.ts
@@ -10,7 +10,6 @@ export type Event = {
     price: number
 }
 
-//NOTE: gets faked once module load
 let events: Event[] = []
 
 export type EventsPageData = {
@@ -18,6 +17,8 @@ export type EventsPageData = {
 }
 
 export const eventsPageLoader = async () => {
+    // NOTE: the `fakeEvents` call needs to stay within the scope of the loader function
+    // this ensures that loader deps are tree shaked, and only included in the server bundle
     if (!events.length) {
         events = fakeEvents(50)
     }
